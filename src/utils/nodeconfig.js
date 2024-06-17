0f289a5bdfeec17321128a1a5d5832dd33bc73f8
0rigin-c0de <shunnu807@gmail.com>
2024-06-17 21:32:37 +0530

    helper functions

---
+++ src/utils/nodeconfig.js
@@ -0,0 +1,27 @@
+import { CustomLink, TargetNode, OriginNode } from '../elements'
+
+
+const createEntity = (id, x, y, label, type) => ({
+  id: id.toString(),
+  position: { x, y },
+  data: { label },
+  type,
+});
+
+export const initialNodeSetup = [
+  createEntity(1, -300, 100, "Origin 1", "origin-node"),
+  createEntity(2, -400, 200, "Origin 2", "origin-node"),
+  createEntity(4, 500, 100, "Target 1", "target-node"),
+  createEntity(5, 500, 400, "Target 2", "target-node"),
+]
+
+export const initialLinkSetup = [];
+
+export const nodeCategories = {
+  "origin-node": OriginNode,
+  'target-node': TargetNode,
+}
+
+export const linkCategories = {
+  'custom-link': CustomLink,
+}
