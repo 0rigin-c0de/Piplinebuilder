d6990192029c7116e77ae3997344feda944b4f2b
0rigin-c0de <shunnu807@gmail.com>
2024-06-17 22:57:28 +0530

    initial-commit

--- src/utils/nodeconfig.js
+++ src/utils/nodeconfig.js
@@ -11,6 +11,7 @@ const createEntity = (id, x, y, label, type) => ({
 export const initialNodeSetup = [
   createEntity(1, -300, 100, "Origin 1", "origin-node"),
   createEntity(2, -400, 200, "Origin 2", "origin-node"),
+  createEntity(3, -500, 300, "Origin 3", "origin-node"),
   createEntity(4, 500, 100, "Target 1", "target-node"),
   createEntity(5, 500, 400, "Target 2", "target-node"),
 ]
